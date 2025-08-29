84cdd886407a67a5136ef0cf363178e65f12ea7b
SureshAdabala <sureshadabala0836@gmail.com>
2025-08-29 15:53:13 +0530

    ExtraFiles

---
+++ Day_18/SkiptheDigit5.java
@@ -0,0 +1,17 @@
+
+public class SkiptheDigit5 {
+    public static void main(String[] args) 
+    {
+         //Program to print the  number which are not having '5'
+        for(int i=1;i<=60;i++)
+        {
+            int n=i;
+            for(;n>0;){
+                int digit = n%10;
+                if(digit == 5) break;
+                n/=10;
+        }if(n==0)
+        System.out.print(i+" ");
+        }
+    }
+}
