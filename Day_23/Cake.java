89c20b9db332f0b9c75aaf6b6ce465d4e533e0b0
SureshAdabala <sureshadabala0836@gmail.com>
2025-09-05 17:18:44 +0530

    add

---
+++ Day_23/Cake.java
@@ -0,0 +1,11 @@
+class Cake {
+    String name;
+    String flavour;
+    int kgs;
+    //Constructor
+    Cake(String n,String f,int kg) {
+        name=n;
+        flavour=f;
+        kgs=kg;
+    }
+}
---
+++ Day_23/Friend.java
@@ -0,0 +1,8 @@
+public class Friend {
+    public static void main(String[] args) {
+     Cake obj = new Cake("Babloo","Red Velvet",2);
+     System.out.println(obj.name);
+     System.out.println(obj.flavour);
+     System.out.println(obj.kgs);
+    }
+}
