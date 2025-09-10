82703ef75812d4c6935c0b41ebe54373116c8cc6
SureshAdabala <sureshadabala0836@gmail.com>
2025-09-10 17:46:46 +0530

    AddcopyConstructorFIles

---
+++ Day_25/SqaureBox.java
@@ -0,0 +1,24 @@
+package Java_Programs;
+
+import java.util.Scanner;
+
+public class SqaureBox {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a Number: ");
+		int num = sc.nextInt();
+		int count = 0;
+		for (int i = 1; i <= num * num; i++) {
+			count++;
+			System.out.print(" *");
+			if (count == num) {
+				System.out.println();
+				count = 0;
+			}
+
+		}
+		sc.close();
+	}
+}
